68ceb62353ed8137181f56b9487fbb0a8e01fc69
chriskoffie <2425404032@live.gctu.edu.gh>
2026-05-30 23:07:41 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Chris Armah Koffie
+# STUDENT ID: 2425404032
+# Email: 2425404032@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Chris Armah Koffie, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # This main function prints my initials in large stylized letters.
+    print()
+    print("...CAK")
+    print()
+    print("...CCCCCCCCCC......AAAAAAAAAA.......K........KK...")
+    print("...CCCCCCCCCCCC...AA........AA......K......KK.....")
+    print("...CC............AA..........AA.....K....KK.......")
+    print("...CC............AA..........AA.....K..KK.........")
+    print("...CC............AAAAAAAAAAAAAA.....KKK...........")
+    print("...CC............AA..........AA.....K..KK.........")
+    print("...CC............AA..........AA.....K....KK.......")
+    print("...CCCCCCCCCCCC..AA..........AA.....K......KK.....")
+    print("...CCCCCCCCCC.**.AA..........AA.**..K........KK.**")
+    print("...CCCCCCCCCC.**.AA..........AA.**..K........KK.**")
+    print()
 
 
 main()
